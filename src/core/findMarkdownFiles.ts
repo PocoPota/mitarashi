15bfb262569bb42bdb31e1ec8324950494e023ef
pocopota <pocopota.program@gmail.com>
2025-07-31 12:09:48 +0900

    add findMarkdownFIles

---
+++ src/core/findMarkdownFiles.ts
@@ -0,0 +1,19 @@
+import fs from "fs/promises";
+import path from "path";
+
+export async function findMarkdownFiles(dir: string): Promise<string[]> {
+  const entries = await fs.readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(
+    entries.map(async (entry) => {
+      const fullPath = path.join(dir, entry.name);
+      if (entry.isDirectory()) {
+        return findMarkdownFiles(fullPath);
+      } else if (entry.isFile() && entry.name.endsWith(".md")) {
+        return [fullPath];
+      } else {
+        return [];
+      }
+    })
+  );
+  return files.flat();
+}
