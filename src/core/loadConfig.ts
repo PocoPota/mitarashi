040b79931d38d0256ded4a1e8d8a6e7f0c0797ee
pocopota <pocopota.program@gmail.com>
2025-07-31 11:34:47 +0900

    add loadConfig.ts

---
+++ src/core/loadConfig.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import { MitarashiConfig } from "types";
+import { pathToFileURL } from "url";
+
+export async function loadCondig(rootDir: string): Promise<MitarashiConfig> {
+  const configPath = path.resolve(rootDir, "mitarashi.config.ts");
+  const module = await import(pathToFileURL(configPath).href);
+  return module.default;
+}
